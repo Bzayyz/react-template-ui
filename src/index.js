af19cfd6d8904fd8defda48882c365218cac8c6e
jinli.lyz <jinli.lyz@taobao.com>
2024-01-15 11:22:06 +0800

    feat: add meticulous ai test script

--- src/index.js
+++ src/index.js
@@ -7,11 +7,27 @@ import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './store'
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader'
+
+async function startApp() {
+  // Record all sessions on localhost, staging stacks and preview URLs
+
+  // Start the Meticulous recorder before you initialise your app.
+  // Note: all errors are caught and logged, so no need to surround with try/catch
+  await tryLoadAndStartRecorder({
+    projectId: 'Uaxu4O9UYv2I79zFhcQiWwKuJEd6zBkcMVw00uWp',
+    isProduction: false,
+  })
+
+  // Initalise app after the Meticulous recorder is ready, e.g.
+  createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  )
+}
+
+startApp()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
